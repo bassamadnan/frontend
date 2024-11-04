db8fc19852e2af70425fd2243fe5fc605d7851ca
bassamadnan <bassamadnan2012@gmail.com>
2024-11-04 07:28:38 +0530

    update sidebar items

--- src/components/listItems.js
+++ src/components/listItems.js
@@ -2,87 +2,73 @@ import React from 'react';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
+import Tooltip from '@mui/material/Tooltip';
 import HomeIcon from '@mui/icons-material/Home';
 import AppsIcon from '@mui/icons-material/Apps';
-import SettingsIcon from '@mui/icons-material/Settings';
-import SystemUpdateAltIcon from '@mui/icons-material/SystemUpdateAlt';
-import AccountTreeIcon from '@mui/icons-material/AccountTree'; // New icon for Nodes
-
-// import LockIcon from '@mui/icons-material/Lock';
-// import AddToPhotosIcon from '@mui/icons-material/AddToPhotos';
-// import AddToQueueIcon from '@mui/icons-material/AddToQueue';
-
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import AccountTreeIcon from '@mui/icons-material/AccountTree';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import { useNavigate } from 'react-router-dom';
 
 function MainListItems() {
-  const navigate = useNavigate(); // Initialize the useNa vigate hook
+  const navigate = useNavigate();
 
   const handleItemClick = (path) => {
-    navigate(path); // Use navigate to navigate to the specified path
+    navigate(path);
   };
 
   return (
     <>
-      <ListItemButton onClick={() => handleItemClick('/')}>
-        <ListItemIcon>
-          <HomeIcon />
-        </ListItemIcon>
-        <ListItemText primary="Home" />
-      </ListItemButton>
-      <ListItemButton onClick={() => handleItemClick('/add')}>
-        <ListItemIcon>
-          <SettingsIcon />
-        </ListItemIcon>
-        <ListItemText primary="Setup" />
-      </ListItemButton>
-      <ListItemButton onClick={() => handleItemClick('/verticals')}>
-        <ListItemIcon>
-          <AppsIcon />
-        </ListItemIcon>
-        <ListItemText primary="Domains" />
-      </ListItemButton>
-      <ListItemButton onClick={() => handleItemClick('/details?filter=Water Quality')}>
-        <ListItemIcon>
-          <AccountTreeIcon />
-        </ListItemIcon>
-        <ListItemText primary="Nodes" />
-      </ListItemButton>
-      <ListItemButton onClick={() => handleItemClick('/bulk-import')}>
-        <ListItemIcon>
-          <SystemUpdateAltIcon />
-        </ListItemIcon>
-        <ListItemText primary="Bulk Import" />
-      </ListItemButton>
-      {/* <ListItemButton onClick={() => handleItemClick('/addvertical')}>
-        <ListItemIcon>
-          <AddToQueueIcon />
-        </ListItemIcon>
-        <ListItemText primary="Create Vertical" />
-      </ListItemButton>
-      <ListItemButton onClick={() => handleItemClick('/addsensor')}>
-        <ListItemIcon>
-          <AddToQueueIcon />
-        </ListItemIcon>
-        <ListItemText primary="Create Sensor Type" />
-      </ListItemButton>
-      <ListItemButton onClick={() => handleItemClick('/addnode')}>
-        <ListItemIcon>
-          <AddToQueueIcon />
-        </ListItemIcon>
-        <ListItemText primary="Create Node" />
-      </ListItemButton>
-      <ListItemButton onClick={() => handleItemClick('/details')}>
-        <ListItemIcon>
-          <AddToQueueIcon />
-        </ListItemIcon>
-        <ListItemText primary="Nodedata" />
-      </ListItemButton>
-      <ListItemButton onClick={() => handleItemClick('/nodedata')}>
-        <ListItemIcon>
-          <AddToQueueIcon />
-        </ListItemIcon>
-        <ListItemText primary="Nodedata" />
-      </ListItemButton> */}
+      <Tooltip 
+        title="Return to Dashboard" 
+        placement="right"
+        arrow
+      >
+        <ListItemButton onClick={() => handleItemClick('/')}>
+          <ListItemIcon>
+            <HomeIcon />
+          </ListItemIcon>
+          <ListItemText primary="Home" />
+        </ListItemButton>
+      </Tooltip>
+
+      <Tooltip 
+        title="View all domains and their details" 
+        placement="right"
+        arrow
+      >
+        <ListItemButton onClick={() => handleItemClick('/verticals')}>
+          <ListItemIcon>
+            <AppsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Domains" />
+        </ListItemButton>
+      </Tooltip>
+
+      <Tooltip 
+        title="View detailed node information and statistics" 
+        placement="right"
+        arrow
+      >
+        <ListItemButton onClick={() => handleItemClick('/details?filter=Water Quality')}>
+          <ListItemIcon>
+            <AccountTreeIcon />
+          </ListItemIcon>
+          <ListItemText primary="Nodes" />
+        </ListItemButton>
+      </Tooltip>
+
+      <Tooltip 
+        title="Add a new domain or Bulk Import Nodes" 
+        placement="right"
+        arrow
+      >
+        <ListItemButton onClick={() => handleItemClick('/add')}>
+          <ListItemIcon>
+            <AddCircleOutlineIcon />
+          </ListItemIcon>
+          <ListItemText primary="Add Domain" />
+        </ListItemButton>
+      </Tooltip>
     </>
   );
 }
